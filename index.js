ec922d1a6c6246e39c0aa341ffb4239f3bde2435
Axel <lezinaxel@gmail.com>
2017-03-13 16:39:54 +0100

    Ajout dans la base de données en temps réel

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 (function(){
-  var formulaire = document.getElementById("mon-formulaire");
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDBIDtszA7lTAuMdvlJBWtuyDkBKlChL1o",
+      authDomain: "todo-list-59e4a.firebaseapp.com",
+      databaseURL: "https://todo-list-59e4a.firebaseio.com",
+      storageBucket: "todo-list-59e4a.appspot.com",
+      messagingSenderId: "916435756004"
+    };
+    firebase.initializeApp(config);
 
+
+
+  var formulaire = document.getElementById("mon-formulaire");
   formulaire.addEventListener('submit', function(event){
     event.preventDefault();
     var task = document.getElementById("task");
+    var addTask = task.value;
+    console.log(addTask);
+    addToDB(addTask);
   });
+
+function addToDB(data){
+  firebase.database().ref('/items').push().set({
+    id : 4000,
+    createdAt : Date.now(),
+    content : data,
+    status : "fait"
+  });
+}
+
 
 })();
